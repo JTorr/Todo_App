c48436e8d35bb74afc1dc9ceeb8c5bf5c5e6b66d
Julie Torres <reeddetorres@gmail.com>
2014-08-24 09:52:15 -0400

    Add Users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
   match '/help', to: 'static_pages#help', via: 'get'
 
   match '/about', to: 'static_pages#about', via: 'get'
 
+  match '/signup', to: 'users#new', via: 'get'
 
 end
