00a4fec6650d541c4828e4f1654a3ccf91af7573
Julie Torres <reeddetorres@gmail.com>
2014-08-25 17:53:02 -0400

    Create TodosController

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,29 @@
+class TodosController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save!
+      redirect_to todo_index_path, notice: "Item added"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def index
+    @todo = Todo.where(user_id: current_user)
+  end
+
+  private
+
+    def todo_params
+      params.require(:todo, :title, :description), permit(:due_date)
+    end
+end
