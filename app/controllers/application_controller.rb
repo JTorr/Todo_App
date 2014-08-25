7dd3fba077fd7d3161b6e0e4b681bc81c1914b83
Julie Torres <reeddetorres@gmail.com>
2014-08-24 21:33:26 -0400

    Broke some Devise stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:email, :username)}
+    devise_parameter_sanitizer.for(:sign_up) {|u|
+    u.permit(:email, :username, :password, :password_confirmation)}
+  end
 end
